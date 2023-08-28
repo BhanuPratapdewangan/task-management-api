8b59ed4efdb094d527fc415947bb4c65e21835f3
bh@nu77 <bhanupratap04123@gmail.com>
2023-08-28 14:51:45 +0530

    cors

--- index.mjs
+++ index.mjs
@@ -18,7 +18,9 @@ app.use(express.json());
 app.use(cors());
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Origin" , "*",
+    "Access-Control-Allow-Credentials" ,true 
+    )
 });
 
 // User SignUp API's
