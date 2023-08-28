ba4d9854f771af1dd9d8ebb4cbb421d6bd0b812b
bh@nu77 <bhanupratap04123@gmail.com>
2023-08-28 14:11:48 +0530

    cors

--- index.mjs
+++ index.mjs
@@ -17,6 +17,9 @@ const port = process.env.PORT || 3500;
 app.use(express.json());
 app.use(cors());
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+});
 
 // User SignUp API's
 app.post('/signup', async (req, res) => {
